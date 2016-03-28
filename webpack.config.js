ca129b8d245855a20a168e4e3ff0c604f1ef23ce
emguy <emguy.yuzhang@gmail.com>
2016-03-27 22:49:15 -0400

    webpack configuration is updated.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: "babel", query: {presets: ["es2015", "react"]}},
+      { test: /\.jsx?$/,exclude: /node_modules/, loader: "babel", query: {presets: ["es2015", "react"]}},
     ]
   },
   plugins: [
